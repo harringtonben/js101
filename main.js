e9ee018b01a03d2043b8c77ff0eeb76d9ddf1789
ben harrington <harringtonben@icloud.com>
2017-07-24 20:02:29 -0500

    added javascript files and worked how old lesson

---
+++ main.js
@@ -0,0 +1,94 @@
+console.log("I'm in main.js!");
+
+console.log(42);
+
+var numberOfInstructors = 0;
+
+numberOfInstructors = numberOfInstructors + 2;
+
+numberOfInstructors += 2;
+
+
+// JAVASCRIPT VARIABLE TYPES
+// var string = ""
+// var number = 42;
+// var boolean = true/false;
+// var nullvalue = null;
+// var undefvalue;
+// var objvalue = {};
+
+// console.log(numberOfInstructors);
+
+// OPERATOR
+
+// Mathmatical Operator
+
+// var summation = 2 + 3;
+// var sub = 5-1;
+// var multiplication = 4*4;
+// var divide = 9 / 3;
+// var modulus = 7 % 2;
+// console.log(modulus);
+
+// Equality Operators 
+
+// == values are equal
+console.log("4" == 4);
+
+ // **** === // strict equality(value and type)
+console.log("4" === 4); 
+
+// != // values are not equal
+console.log("5" != 4)
+
+// **** !== // values are not strictly equal
+console.log("5" !== 5)
+
+// Relational Operators
+
+// > < >= <=
+
+console.log("we think this should be true:", 8 > 2);
+console.log("we think this should be false:", "a" > "z");
+console.log("we think this should be true:", 8 >= 8);
+
+// Logical Operators
+
+// && (AND)
+// || (OR)
+// ! (NOT)
+
+var num = 2; // true
+console.log(!num); // false
+var zero = 0; // false
+console.log(!zero); // true
+
+// Conditional Logic 
+
+var zoeIsCool = true;
+var callenIsCool = true;
+
+if (zoeIsCool && callenIsCool) {
+ 	console.log("students are happy!")
+ } else if (zoeIsCool || callenIsCool) {
+ 	console.log("something else! ")
+ } else {
+ 	console.log("nope!");
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
