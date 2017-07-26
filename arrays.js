9eab29a74dee5f2c8485e7a96c26b4e8e445a93b
ben harrington <harringtonben@icloud.com>
2017-07-25 19:51:26 -0500

    completed colors exercise

---
+++ arrays.js
@@ -0,0 +1,98 @@
+// console.log("testing");
+
+// var pokemon = "Charmander"
+
+// var array = [42,"string",true,null, {}, pokemon];
+
+// console.log(array[5]);
+// array[33] = "apple";
+
+// console.log(array.length);
+
+
+// // Array methods
+
+// console.log(array.indexOf("string"));
+
+// array.push("cat"); // add value to end
+// console.log(array);
+
+// array.pop(); // remove end value of array
+// console.log(array);		
+
+// array.unshift("zoe is cool"); // add value to beginning of array
+// console.log(array);
+
+// array.shift(); // remove first value of array
+// console.log(array);
+
+// var newString = array.join("|");
+// console.log(newString);
+
+// console.log(typeof newString);
+
+// array = newString.split("|");
+// console.log(array);
+
+// var sentence = "http://www.meetup.com";
+// console.log(sentence.split("/"));
+// console.log(sentence.split(""));
+
+// var newArray = ["dog", "cat", "pig", "hippo"];
+// var numberArray = [26, 77, 4, 15, 8];
+
+// console.log(newArray.reverse());
+
+// console.log(newArray.sort());
+// console.log(numberArray.sort());
+// console.log(numberArray.sort(function(first,second){return first - second}));
+
+// var result = newArray.slice(1, 3); // does not modify array
+// console.log(result);
+
+// console.log(numberArray);
+
+// var result = numberArray.splice(3, 1); // modifies array
+// console.log(result);
+// console.log(numberArray);
+
+// console.log(numberArray[0]);
+
+
+// FOR LOOPS
+
+var newArray = ["dog", "cat", "pig", "hippo"];
+
+for(var i = 0; i < newArray.length; i++) {
+	debugger;
+	console.log("I'm at index " + i);
+	console.log(newArray[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
